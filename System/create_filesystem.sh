9f75238dc4ff7ca61271ba2e6da3ee3db6bad347
Metalaria <gonzalo.mejias.moreno@gmail.com>
2016-01-13 11:58:19 +0100

    Update create_filesystem.sh
    Actualizado el script a la versión 3. Ahora reconoce los discos de manera dinámica, por lo que no están grabados a fuego en el código del script. Por otro lado se han fusionado las funciones de creación de discos para optimizar la ejecución del script.

--- System/create_filesystem.sh
+++ System/create_filesystem.sh
@@ -1,169 +1,131 @@
 #!/bin/bash
-#Version 2
-#Fecha: 23/12/2015
+#Version 3
+#Fecha: 13/01/2016
 #Autor: Gonzalo Mejías Moreno
 
-#echo "¿Servicio a configurar?"
-#read servicio
-#echo $servicio
+########### TO DO #############
+# * Unificar funciones para optimizar
+# * Buscar manera de coger los discos nuevos de manera dinámica
+###############################
 
 echo $1
+	
+# cat /proc/partitions | cut -d " " -f16 | sed '/^$/d'
+# lsblk -fs | cut -d " " -f1,2,3,4 | sed '/^$/d'
+# lsblk -fs | cut -d " " -f1,2,3,4 | sed '/^$/d' | grep -v vg* | grep -v sda | grep -v "NAME" | grep -v "sr0" | head -n 1
+	
+disk_one=`lsblk -fs | cut -d " " -f1,2,3,4 | sed '/^$/d' | grep -v vg* | grep -v sda | grep -v "NAME" | grep -v "sr0" | head -n 1 | tr -d ' '`
+disk_two=`lsblk -fs | cut -d " " -f1,2,3,4 | sed '/^$/d' | grep -v vg* | grep -v sda | grep -v "NAME" | grep -v "sr0" | sed -n '2p' | tr -d ' '`
+disk_three=`lsblk -fs | cut -d " " -f1,2,3,4 | sed '/^$/d' | grep -v vg* | grep -v sda | grep -v "NAME" | grep -v "sr0" | head -n 3 | grep -v "$disk_one" | grep -v "$disk_two" | tr -d ' '`
 
-create_disk_apache_data(){
+part_number=1
+
+create_disk_apache(){
 		#creamos la partición en el disco
-        (echo o; echo n; echo p; echo 1; echo ; echo; echo w) | fdisk /dev/sdb
+        (echo o; echo n; echo p; echo 1; echo ; echo; echo w) | fdisk /dev/$disk_one
+		(echo o; echo n; echo p; echo 1; echo ; echo; echo w) | fdisk /dev/$disk_two
 		#creamos el volumen en el primer disco
-        pvcreate /dev/sdb1
+        pvcreate /dev/$disk_one$part_number | tr -d ' '
+		pvcreate /dev/$disk_two$part_number | tr -d ' '
         #creamos el volumegroup
-        vgcreate apache /dev/sdb1
+        vgcreate apache /dev/$disk_one$part_number | tr -d ' '
+		vgcreate logs_apache /dev/$disk_two$part_number | tr -d ' '
         #Asignamos el tamaño total
         lvcreate -l100%FREE -n lv_apache01 apache
+		lvcreate -l100%FREE -n lv_logs_apache01 logs_apache
 		#Damos formato al disco	
 		mkfs.xfs /dev/apache/lv_apache01
-		#Creamos el punto de montaje
-		mkdir -p /servicios/apache
-		#Añadimos los nuevos puntos de montaje al archivo fstab
-		echo "/dev/apache/lv_apache01 /servicios/apache    xfs     defaults        1 2" >> /etc/fstab
-		#Montamos el nuevo volumen
-		mount -a
-}
-
-create_disk_logs_apache(){
-		#creamos la partición en el disco
-        (echo o; echo n; echo p; echo 1; echo ; echo; echo w) | fdisk /dev/sdc
-		#creamos el volumen en el primer disco
-        pvcreate /dev/sdc1
-		#creamos el volumegroup
-        vgcreate logs_apache /dev/sdc1
-		#Asignamos el tamaño total
-        lvcreate -l100%FREE -n lv_logs_apache01 logs_apache
-		#Damos formato al disco	
 		mkfs.xfs /dev/logs_apache/lv_logs_apache01
 		#Creamos el punto de montaje
+		mkdir -p /servicios/apache
 		mkdir -p /logs/apache
 		#Añadimos los nuevos puntos de montaje al archivo fstab
+		echo "/dev/apache/lv_apache01 /servicios/apache    xfs     defaults        1 2" >> /etc/fstab
 		echo "/dev/logs_apache/lv_logs_apache01 /logs/apache    xfs     defaults        1 2" >> /etc/fstab
 		#Montamos el nuevo volumen
 		mount -a
 }
 
-create_disk_tomcat_data(){
+
+create_disk_tomcat(){
 		#creamos la partición en el disco
-        (echo o; echo n; echo p; echo 1; echo ; echo; echo w) | fdisk /dev/sdb
+        (echo o; echo n; echo p; echo 1; echo ; echo; echo w) | fdisk /dev/$disk_one
+		(echo o; echo n; echo p; echo 1; echo ; echo; echo w) | fdisk /dev/$disk_two
 		#creamos el volumen en el primer disco
-        pvcreate /dev/sdb1
+        pvcreate /dev/$disk_one$part_number|tr -d ' '
+		pvcreate /dev/$disk_two$part_number | tr -d ' '
         #creamos el volumegroup
-        vgcreate tomcat /dev/sdb1
+        vgcreate tomcat /dev/$disk_one$part_number|tr -d ' '
+		vgcreate logs_tomcat /dev/$disk_two$part_number | tr -d ' '
         #Asignamos el tamaño total
         lvcreate -l100%FREE -n lv_tomcat01 tomcat
+		lvcreate -l100%FREE -n lv_logs_tomcat01 logs_tomcat
 		#Damos formato al disco	
 		mkfs.xfs /dev/tomcat/lv_tomcat01
+		mkfs.xfs /dev/logs_tomcat/lv_logs_tomcat01
 		#Creamos los puntos de montaje
 		mkdir -p /servicios/tomcat
-		#Añadimos los nuevos puntos de montaje al archivo fstab
-		echo "/dev/tomcat/lv_tomcat01   /servicios/tomcat  xfs     defaults        1 2" >> /etc/fstab
-		#Montamos los nuevos volúmenes
-		mount -a
-}
-
-create_disk_logs_tomcat(){
-		#creamos la partición en el disco
-        (echo o; echo n; echo p; echo 1; echo ; echo; echo w) | fdisk /dev/sdc
-		#creamos el volumen en el primer disco
-        pvcreate /dev/sdc1
-		#creamos el volumegroup
-        vgcreate logs_tomcat /dev/sdc1
-		#Asignamos el tamaño total
-        lvcreate -l100%FREE -n lv_logs_tomcat01 logs_tomcat
-		#Damos formato al disco	
-		mkfs.xfs /dev/logs_tomcat/lv_logs_tomcat01
-		#Creamos el punto de montaje
 		mkdir -p /logs/tomcat
 		#Añadimos los nuevos puntos de montaje al archivo fstab
+		echo "/dev/tomcat/lv_tomcat01   /servicios/tomcat  xfs     defaults        1 2" >> /etc/fstab
 		echo "/dev/logs_tomcat/lv_logs_tomcat01 /logs/tomcat    xfs     defaults        1 2" >> /etc/fstab
-		#Montamos el nuevo volumen
+		#Montamos los nuevos volúmenes
 		mount -a
 }
 
-create_disk_mysql_data(){
+
+create_disk_mysql(){
 		#creamos la partición en el disco
-        (echo o; echo n; echo p; echo 1; echo ; echo; echo w) | fdisk /dev/sdb
+        (echo o; echo n; echo p; echo 1; echo ; echo; echo w) | fdisk /dev/$disk_one
+		(echo o; echo n; echo p; echo 1; echo ; echo; echo w) | fdisk /dev/$disk_two
+		(echo o; echo n; echo p; echo 1; echo ; echo; echo w) | fdisk /dev/$disk_three
 		#creamos el volumen en el primer disco
-        pvcreate /dev/sdb1
+        pvcreate /dev/$disk_one$part_number|tr -d ' '
+		pvcreate /dev/$disk_two$part_number | tr -d ' '
+		pvcreate /dev/$disk_three$part_number | tr -d ' '
         #creamos el volumegroup
-        vgcreate mysql /dev/sdb1
+        vgcreate mysql /dev/$disk_one$part_number|tr -d ' '
+		vgcreate logs_mysql /dev/$disk_two$part_number | tr -d ' '
+		vgcreate backup_mysql /dev/$disk_three$part_number | tr -d ' '
         #Asignamos el tamaño total
         lvcreate -l100%FREE -n lv_mysql01 mysql
+		lvcreate -l100%FREE -n lv_logs_mysql01 logs_mysql
+		lvcreate -l100%FREE -n lv_bakcup_mysql01 backup_mysql
 		#Damos formato al disco	
 		mkfs.xfs /dev/mysql/lv_mysql01
-		#Creamos los puntos de montaje
-		mkdir -p /bd/mybd1
-		#Añadimos los nuevos puntos de montaje al archivo fstab
-		echo "/dev/mysql/lv_mysql01     /bd/mybd1       xfs    defaults        1 2" >> /etc/fstab
-		#Montamos los nuevos volúmenes
-		mount -a
-}
-
-create_disk_mysql_logs(){
-		#creamos la partición en el disco
-        (echo o; echo n; echo p; echo 1; echo ; echo; echo w) | fdisk /dev/sdc
-		#creamos el volumen en el primer disco
-        pvcreate /dev/sdc1
-        #creamos el volumegroup
-        vgcreate logs_mysql /dev/sdc1
-        #Asignamos el tamaño total
-        lvcreate -l100%FREE -n lv_logs_mysql01 logs_mysql
-		#Damos formato al disco	
 		mkfs.xfs /dev/logs_mysql/lv_logs_mysql01
-		#Creamos los puntos de montaje
-		mkdir -p /bd/mylog
-		#Añadimos los nuevos puntos de montaje al archivo fstab
-		echo "/dev/logs_mysql/lv_logs_mysql01    /bd/mylog       xfs    defaults        1 2" >> /etc/fstab
-		#Montamos los nuevos volúmenes
-		mount -a
-}
-
-create_disk_mysql_bakcup(){
-		#creamos la partición en el disco
-        (echo o; echo n; echo p; echo 1; echo ; echo; echo w) | fdisk /dev/sdd
-		#creamos el volumen en el primer disco
-        pvcreate /dev/sdd1
-        #creamos el volumegroup
-        vgcreate backup_mysql /dev/sdd1
-        #Asignamos el tamaño total
-        lvcreate -l100%FREE -n lv_bakcup_mysql01 backup_mysql
-		#Damos formato al disco	
 		mkfs.xfs /dev/backup_mysql/lv_bakcup_mysql01
 		#Creamos los puntos de montaje
+		mkdir -p /bd/mybd1
+		mkdir -p /bd/mylog
 		mkdir -p /bd/mybackup
 		#Añadimos los nuevos puntos de montaje al archivo fstab
+		echo "/dev/mysql/lv_mysql01     /bd/mybd1       xfs    defaults        1 2" >> /etc/fstab
+		echo "/dev/logs_mysql/lv_logs_mysql01    /bd/mylog       xfs    defaults        1 2" >> /etc/fstab
 		echo "/dev/backup_mysql/lv_bakcup_mysql01    /bd/mybackup       xfs    defaults        1 2" >> /etc/fstab
 		#Montamos los nuevos volúmenes
 		mount -a
 }
 
+
 case $1 in
 	"apache")
 		echo "apache"
-		create_disk_apache_data
-		create_disk_logs_apache
+		create_disk_apache
 		lsblk -f
-		df -h
+		df -Th
 		exit 0
 	;;
 	"tomcat")
-		create_disk_logs_tomcat
-		create_disk_tomcat_data
+		create_disk_tomcat
 		lsblk -f
-		df -h
+		df -Th
 		exit 0
 	;;
 	"mysql")
-		create_disk_mysql_data
-		create_disk_mysql_logs
-		create_disk_mysql_bakcup
+		create_disk_mysql
 		lsblk -f
-		df -h
+		df -Th
 		exit 0
 	;;
 	*)
