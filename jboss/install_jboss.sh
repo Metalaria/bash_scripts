29a6ad3afe3a266b48ef3ba6e3e5344ed5e7362e
Metalaria <gonzalo.mejias.moreno@gmail.com>
2016-01-26 13:08:13 +0100

    Cambiado y modificado el script de inicio

--- jboss/install_jboss.sh
+++ jboss/install_jboss.sh
@@ -167,41 +167,150 @@ function installjboss {
 
   #Nuevo script de inicio	
   echo <<INITSCRIPTEOF >$INITSCRIPT '
-#!/bin/sh
-### BEGIN INIT INFO
-# Reference: http://stackoverflow.com/questions/6880902/start-jboss-7-as-a-service-on-linux
-# Provides:          jboss
-# Required-Start:    $local_fs $remote_fs $network $syslog
-# Required-Stop:     $local_fs $remote_fs $network $syslog
-# Default-Start:     2 3 4 5
-# Default-Stop:      0 1 6
-# Short-Description: Start/Stop JBoss AS v7.0.0
-### END INIT INFO
-#
-#source some script files in order to set and export environmental variables
-#as well as add the appropriate executables to $PATH
-#[ -r /etc/profile.d/java.sh ] && . /etc/profile.d/java.sh
-#[ -r /etc/profile.d/jboss.sh ] && . /etc/profile.d/jboss.sh
+	#!/bin/sh
 
-export JBOSS_HOME=/servicios/jboss/
+# Load JBoss AS init.d configuration.
+if [ -z "$JBOSS_CONF" ]; then
+  JBOSS_CONF=/servicios/jboss/bin/init.d/jboss-as.conf
+fi
+
+[ -r "$JBOSS_CONF" ] && . "${JBOSS_CONF}"
+
+if [ -z "$JBOSS_HOME" ]; then
+  JBOSS_HOME=/servicios/jboss
+fi
+export JBOSS_HOME
+
+if [ -z "JBOSS_MODE" ]; then
+  JBOSS_MODE=standalone
+fi
+
+if [ -z "$JBOSS_PIDFILE" ]; then
+  JBOSS_PIDFILE=/var/run/jboss-as/jboss-as-standalone.pid
+fi
+export JBOSS_PIDFILE
+
+if [ -z "$JBOSS_CONSOLE_LOG" ]; then
+  JBOSS_CONSOLE_LOG=/logs/jboss/console.log
+fi
+
+if [ -z "$STARTUP_WAIT" ]; then
+  STARTUP_WAIT=30
+fi
+
+if [ -z "$SHUTDOWN_WAIT" ]; then
+  SHUTDOWN_WAIT=30
+fi
+
+if [ -z "$JBOSS_CONFIG" ]; then
+  JBOSS_CONFIG=standalone.xml
+fi
+
+if [ -z "$JBOSS_USER" ]; then
+  JBOSS_USER=jboss
+fi
+
+JBOSS_SCRIPT=$JBOSS_HOME/bin/$JBOSS_MODE.sh
+
+prog='jboss-as'
+
+CMD_PREFIX=''
+
+start() {
+  echo -n "Starting $prog: "
+  if [ -f $JBOSS_PIDFILE ]; then
+    read ppid < $JBOSS_PIDFILE
+    if [ `ps --pid $ppid 2> /dev/null | grep -c $ppid 2> /dev/null` -eq '1' ]; then
+      echo -n "$prog is already running"
+      echo "Failure"
+      echo
+      return 1
+    else
+      rm -f $JBOSS_PIDFILE
+    fi
+  fi
+  mkdir -p $(dirname $JBOSS_CONSOLE_LOG)
+  cat /dev/null > $JBOSS_CONSOLE_LOG
+
+  mkdir -p $(dirname $JBOSS_PIDFILE)
+  #echo "chown -R /servicios/jboss"
+  #chown jboss -R /servicios/jboss || true
+  #$CMD_PREFIX JBOSS_PIDFILE=$JBOSS_PIDFILE $JBOSS_SCRIPT 2>&1 > $JBOSS_CONSOLE_LOG &
+  #$CMD_PREFIX JBOSS_PIDFILE=$JBOSS_PIDFILE $JBOSS_SCRIPT &
+
+  if [ ! -z "$JBOSS_USER" ]; then
+      su $JBOSS_USER  LAUNCH_JBOSS_IN_BACKGROUND=1 JBOSS_PIDFILE=$JBOSS_PIDFILE $JBOSS_SCRIPT  $JBOSS_CONFIG 2>&1 > $JBOSS_CONSOLE_LOG &
+  fi
+
+  count=0
+  launched=false
+
+  until [ $count -gt $STARTUP_WAIT ]
+  do
+    grep 'JBoss AS.*started in' $JBOSS_CONSOLE_LOG > /dev/null
+    if [ $? -eq 0 ] ; then
+      launched=true
+      break
+    fi
+    sleep 1
+        let count=$count+1;
+  done
+
+  echo "Success"
+  echo
+  return 0
+}
+
+stop() {
+  echo -n $"Stopping $prog: "
+  count=0;
+
+  if [ -f $JBOSS_PIDFILE ]; then
+    read kpid < $JBOSS_PIDFILE
+    let kwait=$SHUTDOWN_WAIT
+
+    # Try issuing SIGTERM
+
+    kill -15 $kpid
+    until [ `ps --pid $kpid 2> /dev/null | grep -c $kpid 2> /dev/null` -eq '0' ] || [ $count -gt $kwait ]
+    do
+      sleep 1
+      let count=$count+1;
+    done
+
+    if [ $count -gt $kwait ]; then
+      kill -9 $kpid
+    fi
+  fi
+  rm -f $JBOSS_PIDFILE
+  echo "Success"
+  echo
+}
 
 case "$1" in
-    start)
-        echo "Starting JBoss AS 7.0.0"
-        sudo -u jboss sh ${JBOSS_HOME}/bin/standalone.sh > /dev/null 2>&1 &
-    ;;
-    stop)
-        echo "Stopping JBoss AS 7.0.0"
-        sudo -u jboss sh ${JBOSS_HOME}/bin/jboss-admin.sh --connect command=:shutdown > /dev/null 2>&1 &
-    ;;
-    *)
-        echo "Usage: /etc/init.d/jboss {start|stop}"
-        exit 1
-    ;;
-esac
-
+start)
+echo "Starting JBoss AS7..."
+start
+;;
+stop)
+echo "Stopping JBoss AS7..."
+stop
+;;
+log)
+echo "Showing server.log..."
+tail -1000f /logs/jboss/standalone/log/server.log
+;;
+console)
+echo "Showing console.log..."
+tail -1000f ${JBOSS_CONSOLE_LOG}
+;;
+*)
+echo "Usage: /etc/init.d/jboss {start|stop|log|console}"
+exit 1
+;; esac
 exit 0
-'
+
+' 
 INITSCRIPTEOF
 
   chmod +x $INITSCRIPT
@@ -281,7 +390,7 @@ function installmysqlconnector {
     local MYSQLCONNECTORTARGETDIR="$INSTALLTARGET/modules/com/mysql/main"
     mkdir -p $MYSQLCONNECTORTARGETDIR
     cd $MYSQLCONNECTORTARGETDIR
-    wget -O - "$MYSQLCONNECTORURL" | tar xz
+    wget -O - "$MYSQLCONNECTORURL" | tar xJf
     echo <<EOFMODULE >$MYSQLCONNECTORTARGETDIR/module.xml '
 <?xml version="1.0" encoding="UTF-8"?>
  
